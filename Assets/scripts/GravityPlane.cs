a5db6698746e67050cf597558ecceeb8ed4e35d7
monaday <mona@simplyphp.com>
2025-01-03 10:30:02 -0500

    add basic scripts

---
+++ Assets/scripts/GravityPlane.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GravityPlane<T> 
+{
+
+    public T points {get,set}
+    public T wells {get, set}
+    public int Rows { get; private set; }
+    public int Columns { get; private set; }
+
+    // constructor
+    public GravityPlane(int height, int width, float res) {
+        // generate an array of empty points
+        rows = int weight / res
+        cols = int height / res
+        
+    }
+
+    public void placeWells(list wells) {
+
+    }
+
+    public Point getNearest() {
+
+    }
+}
