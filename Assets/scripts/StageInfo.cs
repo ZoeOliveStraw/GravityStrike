62222e682b1ce219b4398159d2d094e0ea783aa6
ZoeOliveStraw <zoe.olive.straw@gmail.com>
2025-01-03 17:58:58 -0500

    Provided properties and constructors for StageInfo class

--- Assets/scripts/StageInfo.cs
+++ Assets/scripts/StageInfo.cs
@@ -1,6 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class StageInfo
 {
+    public int LevelWidth;
+    public int LevelHeight;
+    public List<Well> Wells;
+    public List<Vector2> EnemyLocations;
+
+    public StageInfo(int width, int height)
+    {
+        LevelWidth = width;
+        LevelHeight = height;
+    }
     
+    public StageInfo(int width, int height, List<Well> wells)
+    {
+        LevelWidth = width;
+        LevelHeight = height;
+        Wells = wells;
+    }
+    
+    public StageInfo(int width, int height, List<Well> wells, List<Vector2> enemies)
+    {
+        LevelWidth = width;
+        LevelHeight = height;
+        Wells = wells;
+        EnemyLocations = enemies;
+    }
 }
