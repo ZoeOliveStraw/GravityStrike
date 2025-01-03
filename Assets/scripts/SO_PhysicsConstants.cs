2d00f73494636ec542c82b6bf4ea641932e8a72f
ZoeOliveStraw <zoe.olive.straw@gmail.com>
2025-01-03 17:44:36 -0500

    Defined some new scripts, yet to finish them

---
+++ Assets/scripts/SO_PhysicsConstants.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class SO_PhysicsConstants : ScriptableObject
+{
+    [SerializeField] public float GravityStrength;
+    [SerializeField] public float GravityRangePerMass;
+}
