30b99490b9cafe500eac055aff71a00b6dc07e81
ZoeOliveStraw <zoe.olive.straw@gmail.com>
2025-01-03 19:18:50 -0500

    Methods in SO_DIfficultyProfiles

--- Assets/scripts/SO_DifficultyProfile.cs
+++ Assets/scripts/SO_DifficultyProfile.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 public class SO_DifficultyProfile : ScriptableObject
 {
     [SerializeField] private Vector2Int starCountRange;
-    [SerializeField] private Vector2Int starSizeRange;
+    [SerializeField] private Vector2 starSizeRange;
     [SerializeField] private Vector2Int enemyCountRange;
+
+    public int GetStarCount()
+    {
+        return Random.Range(starCountRange.x, starCountRange.y);
+    }
+
+    public float GetStarSize()
+    {
+        return Random.Range(starSizeRange.x, starSizeRange.y);
+    }
+
+    public int GetEnemyCount()
+    {
+        return Random.Range(enemyCountRange.x, enemyCountRange.y);
+    }
 }
