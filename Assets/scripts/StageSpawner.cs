03ee0d7aa8884dfc4edfc3ec79a89b11934c9854
ZoeOliveStraw <zoe.olive.straw@gmail.com>
2025-01-04 20:43:40 -0500

    Final push of night

--- Assets/scripts/StageSpawner.cs
+++ Assets/scripts/StageSpawner.cs
@@ -58,7 +58,7 @@ public class StageSpawner : MonoBehaviour
     
     private Color MapMagnitudeToColor(float magnitude)
     {
-        float normalizedMagnitude = Mathf.Clamp01(magnitude / 50f);
+        float normalizedMagnitude = Mathf.Clamp01(magnitude / 20f);
         return Color.Lerp(GravityColor0, GravityColor1, normalizedMagnitude);
     }
 
