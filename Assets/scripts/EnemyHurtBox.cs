e01f2851ad6cdaa561742942a24299dc543babdf
monaday <monaday@daydevelops.com>
2025-01-05 12:25:34 -0500

    initialized hitbox for enemy

---
+++ Assets/scripts/EnemyHurtBox.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class EnemyHurtBox : MonoBehaviour
+{
+
+    [SerializeField] private GameObject explosion;
+    private void Start()
+    {
+    }
+
+
+    public void death() {
+        // initialize particle field
+        // decrement count
+        // delete enemy
+    }
+}
